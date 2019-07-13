508422c559ba75cf02b0308c304616c85c57a358
Max Kovalenko <mxss1998@yandex.ru>
2019-07-13 19:38:04 +0300

    Parameter field in validator rules is not optional

--- DL/Module/Validator/IValidatorRule.cs
+++ DL/Module/Validator/IValidatorRule.cs
@@ -1,11 +1,8 @@
 using BaseFramework.DL.Module.Http;
 using Nancy;
-using Newtonsoft.Json.Linq;
 
 namespace BaseFramework.DL.Module.Validator {
     public interface IValidatorRule {
-        string Parameter { get; }
-        
         HttpError Process(Request request);
     }
 }
