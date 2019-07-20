6b8454c32b0b34da980a90f7f46d9dfb09fc4cdf
Max Kovalenko <mxsshere@gmail.com>
2019-07-20 13:23:03 +0300

    added transactions functionality to DBConnection
    Signed-off-by: Max Kovalenko <mxsshere@gmail.com>

--- DL/Module/Db/DbConnection.cs
+++ DL/Module/Db/DbConnection.cs
@@ -6,10 +6,13 @@ namespace BaseFramework.DL.Module.Db {
     public class DbConnection {
         private readonly IDbConnection _connection;
 
+        private IDbTransaction _lastTransaction;
+        
         private static DbConnection _instance;
 
         private DbConnection() {
             _connection = new NpgsqlConnection(AppConfig.Get().GetConnectionString());
+            _connection.Open();
         }
 
         public static DbConnection Get() {
@@ -18,6 +21,15 @@ namespace BaseFramework.DL.Module.Db {
             }
 
             return _instance;
+        }
+
+        public static IDbTransaction BeginTransaction() {
+            Get()._lastTransaction = Connection().BeginTransaction();
+            return Get()._lastTransaction;
+        }
+        
+        public static void RollbackTransaction() {
+            Get()._lastTransaction.Rollback();
         }
 
         public static IDbConnection Connection() => Get()._connection;
