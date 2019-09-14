36ef022a3625f59bee93bc9ce1fcf1a83ecb6a3e
Max Kovalenko <mxsshere@gmail.com>
2019-09-13 20:17:50 -0400

    removed user model (to avoid conflicts)

--- DL/Middleware/Auth/JwtMiddleware.cs
+++ DL/Middleware/Auth/JwtMiddleware.cs
@@ -1,23 +1,19 @@
 using BaseFramework.DL.Module.Auth;
 using BaseFramework.DL.Module.Http;
-using BaseFramework.DL.Repository.User;
 using Nancy;
 
 namespace BaseFramework.DL.Middleware.Auth {
     public class JwtMiddleware : IMiddleware {
         public ProcessedRequest Process(ProcessedRequest request) {
             var userId = Jwt.GetUserIdFromToken((string) request.Request.Query["api_token"] ?? "");
-
-            var user = UserRepository.Find(userId);
-
-            if (user != null) {
-                request.UserId = user.id;
-            }
-            else {
+            
+            if (userId == 0) {
                 request.AddError(
                     new HttpError(HttpStatusCode.Unauthorized, "Invalid api_token")
                 );
             }
+
+            request.UserId = userId;
 
             return request;
         }
